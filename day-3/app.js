79617cd798a0faedd6eaae5b05577d9b122f606f
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-22 22:29:15 +0600

    fetch without await

--- day-3/app.js
+++ day-3/app.js
@@ -1,17 +1,26 @@
 const button = document.querySelector('.container button');
 const jokeDiv = document.querySelector('.container .joke p');
 
-document.addEventListener('DOMContentLoaded', getJock)
+document.addEventListener('DOMContentLoaded', getJoke)
 
-button.addEventListener('click', getJock);
+button.addEventListener('click', getJoke);
 
-async function getJock(){
-  const jokeData = await fetch('https://icanhazdadjoke.com/', {
-    headers:{
+function getJoke(){
+  fetch('https://icanhazdadjoke.com/', {
+    headers: {
       'Accept': 'application/json'
-    }
-  });
-  const jokeObj = await jokeData.json();
-  jokeDiv.innerHTML = jokeObj.joke;
-  console.log(jokeData);
+    }})
+    .then(data => data.json())
+    .then(obj => jokeDiv.innerHTML = obj.joke);
 }
+
+// async function getJoke(){
+//   const jokeData = await fetch('https://icanhazdadjoke.com/', {
+//     headers:{
+//       'Accept': 'application/json'
+//     }
+//   });
+//   const jokeObj = await jokeData.json();
+//   jokeDiv.innerHTML = jokeObj.joke;
+//   console.log(jokeData);
+// }
