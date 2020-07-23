5a3b1f355cea301cfaed1c5e0c9841666b9f4ab3
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-24 05:30:14 +0600

    adding day-4 with cross domain

--- day-4/app.js
+++ day-4/app.js
@@ -1,11 +1,19 @@
+
 const form = document.querySelector('form');
 const factDiv = document.querySelector('.number-fact');
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const baseURL = "https://numbersapi.com/";
+  const loadText ="wait a little bit ⌛";
+  factDiv.innerHTML = loadText;
+  const baseURL = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/";
   const number = e.target.querySelector('input[type="number"]').value;
-  fetch(baseURL+number)
-    .then(response=> response.text())
-    .then(text => console.log(text)) ;
-})
+  fetch(baseURL + number,{
+    headers:{
+      'x-requested-with': 'text/plain'
+    }
+  })
+    .then(response => response.text())
+    .then(text => factDiv.innerHTML = text)
+    .catch(e=>console.log(e));
+}) 
