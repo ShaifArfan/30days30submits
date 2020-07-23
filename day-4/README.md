9733a98a5dce04f2f0154addfd7effe8bbd96fec
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-24 04:29:02 +0600

    adding root index file

---
+++ day-4/README.md
@@ -0,0 +1,3 @@
+## Submit Name: Number Api
+
+API URL: http://numbersapi.com/
--- day-4/app.js
+++ day-4/app.js
@@ -3,9 +3,9 @@ const factDiv = document.querySelector('.number-fact');
 
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
-  const baseURL = "http://numbersapi.com/";
+  const baseURL = "https://numbersapi.com/";
   const number = e.target.querySelector('input[type="number"]').value;
   fetch(baseURL+number)
     .then(response=> response.text())
-    .then(text => factDiv.innerHTML = text) ;
+    .then(text => console.log(text)) ;
 })
