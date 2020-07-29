2deed2676600f73cd3dd7f859941e0b7e5696cc4
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-07-29 06:01:54 +0600

    adding day-9

---
+++ day-9/main.js
@@ -0,0 +1,12 @@
+const readMoreBtn = document.querySelector('.readMoreBtn');
+const text = document.querySelector('.text');
+
+readMoreBtn.addEventListener('click',(e)=>{
+  text.classList.toggle('show-more')
+  readMoreBtn.classList.toggle('less');
+  if(readMoreBtn.innerText === 'Read More'){
+    readMoreBtn.innerText = 'Read Less';
+  }else{
+    readMoreBtn.innerText = 'Read More';
+  }
+})
